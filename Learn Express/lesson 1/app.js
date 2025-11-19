870e815a017db09110f6334855b6b8760f2c9771
Saied53 <saydurrahman3952@gmail.com>
2025-11-19 19:49:46 +0600

    test get, put, delete, post with postman

--- Learn Express/lesson 1/app.js
+++ Learn Express/lesson 1/app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res)=>{
-    res.send("<h1>Hi, I am a get method of express js</h1>")
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hi, I'm a get route</h1>");
+});
+
+app.post("/", (req, res) => {
+  res.send("<h1>Hi, I'm a post route</h1>");
+});
+
+app.delete("/", (req, res) => {
+  res.send("<h1>Hi, I'm a delete route</h1>");
+});
+
+app.put("/", (req, res) => {
+  res.send("<h1>Hi, I'm a put route</h1>");
+});
 
 module.exports = app;
