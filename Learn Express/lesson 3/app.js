017ca8ecdf0c3dc895d72e7460eae1a9617e568b
Saied53 <saydurrahman3952@gmail.com>
2025-11-22 15:05:47 +0600

    leasson 3 completed

--- Learn Express/lesson 3/app.js
+++ Learn Express/lesson 3/app.js
@@ -3,16 +3,29 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const userRouter = require("../Routers/users.route");
+// const userRouter = require("../Routers/users.route");
+
+
 
 app.use("/register", (req, res) => {
   res.statusCode = 200;
   res.sendFile(path.join(__dirname, 'register.html'));
 });
 
+
+app.get("/login", (req, res) => {
+  res.cookie("name", "SaydurRahman");
+  res.cookie("age", "30");
+
+  res.append("ID", "123");
+
+  res.clearCookie("name");
+
+  res.end();
+});
+
 app.use("/", (req, res) => {
   res.statusCode = 200;
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-
 module.exports = app;
