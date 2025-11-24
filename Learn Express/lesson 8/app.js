ea16487113162e61f4ba685ce507784374c9b351
Saied53 <saydurrahman3952@gmail.com>
2025-11-24 20:41:39 +0600

    regular express problem solve

--- Learn Express/lesson 8/app.js
+++ Learn Express/lesson 8/app.js
@@ -2,9 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+
 app.get("/product/:id", (req, res) => {
-  res.send(`Product is id:  ${req.params.id}`);
+  const { id } = req.params;
+  
+  // Validate if id is numeric
+  if (!/^\d+$/.test(id)) {
+    return res.status(400).send({ message: "Invalid product ID" });
+  }
+  
+  res.send(`Product ID: ${id}`);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
