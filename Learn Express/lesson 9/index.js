0a0d9d2e737bea2936928cd094b31a3aa0687cfa
Saied53 <saydurrahman3952@gmail.com>
2025-11-25 15:39:31 +0600

    evn ignore

--- Learn Express/lesson 9/index.js
+++ Learn Express/lesson 9/index.js
@@ -1,12 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
-
-
-app.get("/", (req, res)=>{
-    res.send(`Hi, welcome another lession`)
-})
+app.get("/", (req, res) => {
+  res.send(`Hi, welcome another lession`);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
